6e687c95c991bc9dfb0dd1041b236fb176ca74ee
biohack5079 <biohack5079@gmail.com>
2025-12-13 11:27:51 +0900

    changed

--- cnc/urls.py
+++ cnc/urls.py
@@ -8,9 +8,9 @@ def get_stripe_public_key(request):
     return JsonResponse({'publicKey': settings.STRIPE_PUBLISHABLE_KEY})
 
 urlpatterns = [
-    path("api/get_vapid_public_key/", views.VapidPublicKeyView.as_view(), name="get_vapid_public_key"),
-    path("api/save_push_subscription/", views.SavePushSubscriptionView.as_view(), name="save_push_subscription"),
-    path('api/stripe/public-key/', get_stripe_public_key, name='get_stripe_public_key'),
-    path("api/create-checkout-session/", views.CreateCheckoutSessionView.as_view(), name="create_checkout_session"),
-    path("api/stripe-webhook/", views.StripeWebhookView.as_view(), name="stripe_webhook"),
+    path("get_vapid_public_key/", views.VapidPublicKeyView.as_view(), name="get_vapid_public_key"),
+    path("save_push_subscription/", views.SavePushSubscriptionView.as_view(), name="save_push_subscription"),
+    path('stripe/public-key/', get_stripe_public_key, name='get_stripe_public_key'),
+    path("create-checkout-session/", views.CreateCheckoutSessionView.as_view(), name="create_checkout_session"),
+    path("stripe-webhook/", views.StripeWebhookView.as_view(), name="stripe_webhook"),
 ]
