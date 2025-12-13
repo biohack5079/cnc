6fd6a536df2e4c438d10c995a541863f45a71ea4
biohack5079 <biohack5079@gmail.com>
2025-12-13 13:05:56 +0900

    Fix urls.py to resolve AttributeError

--- cybernetcall/urls.py
+++ cybernetcall/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("cnc.urls")), # cnc.urlsをルートに含める
+    # cnc.urls をルートに含める
+    path("", include("cnc.urls")),
 ]
