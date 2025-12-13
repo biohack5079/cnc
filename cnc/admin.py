2cfb3e27ae4fb7373b827fc706d52cd39deb447a
biohack5079 <biohack5079@gmail.com>
2025-12-13 11:38:37 +0900

    cncアプリの初期マイグレーションを追加

--- cnc/admin.py
+++ cnc/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import UserProfile, PushSubscription, StripeCustomer
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'subscription_status', 'created_at')
+    search_fields = ('id', 'stripe_customer_id', 'stripe_subscription_id')
+    list_filter = ('subscription_status', 'created_at')
+    readonly_fields = ('id', 'created_at')
+
+@admin.register(PushSubscription)
+class PushSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user_uuid', 'endpoint')
+    search_fields = ('user_uuid',)
+
+@admin.register(StripeCustomer)
+class StripeCustomerAdmin(admin.ModelAdmin):
+    list_display = ('user_uuid', 'stripe_customer_id')
+    search_fields = ('user_uuid', 'stripe_customer_id')
